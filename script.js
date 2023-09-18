64d9604671c2886ca9be3779101e801cd5e6b0c8
NewHellequin <tom.mightysloth.mills@gmail.com>
2023-09-18 21:22:38 +0100

    Addition of dice roll function

--- script.js
+++ script.js
@@ -3,9 +3,37 @@
 // Selecting elements
 const scoreElement0 = document.querySelector('#score--0');
 const scoreElement1 = document.getElementById('score--1');
+const currentElement0 = document.getElementById('current--0');
+const currentElement1 = document.getElementById('current--1');
+
 const diceElement1 = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
 scoreElement0.textContent = 0;
 scoreElement1.textContent = 0;
 diceElement1.classList.add('hidden');
+
+let currentScore = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generate a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  // 2. Display dice
+  diceElement1.classList.remove('hidden');
+  diceElement1.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    // add dice to current score
+    currentScore = currentScore + dice;
+    currentElement0.textContent = currentScore; //CHANGE LATER
+  } else {
+    // switch to next player
+  }
+});
